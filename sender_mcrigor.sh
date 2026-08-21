6170750e977954b9ea29096129d746201aa0058f
margaretAlexander <98061354+margaretAlexander@users.noreply.github.com>
2026-08-21 09:55:47 -0500

    Update sender_mcrigor.sh
    Updated input variable comment to clarify usage for mcrigor.

--- sender_mcrigor.sh
+++ sender_mcrigor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-input=$(echo $1) # dataset to run metacell on
+input=$(echo $1) # dataset to run mcrigor on (must have ran metacell already and generated cell membership file)
 
 sed "s/input/$input/g" < run_mcrigor.sh | bsub

0d9e65d538da6666e04e444662c879a7ad10c5ac
margaretAlexander <98061354+margaretAlexander@users.noreply.github.com>
2026-08-21 10:03:57 -0500

    Implement filtering logic in sender_metacell.sh
    Added conditional logic to handle filtering based on the filter_lateral parameter.

--- sender_metacell.sh
+++ sender_metacell.sh
@@ -3,8 +3,15 @@ input=$(echo $1) # dataset to run metacell on
 min_size=$(echo $2) # min metacell size
 max_size=$(echo $3) # max metacell size
 interval=$(echo $4) # interval between metacell sizes
+filter_lateral=$(echo $5) # true/false
 
-sed -e "s/input/$input/g" \
-    -e "s/min_size/$min_size/g" \
-    -e "s/max_size/$max_size/g" \
-    -e "s/interval/$interval/g" < run_metacells.sh | bsub
+if [[filter_lateral]]; then
+    sed -e "s/input/$input/g" \
+        -e "s/min_size/$min_size/g" \
+        -e "s/max_size/$max_size/g" \
+        -e "s/interval/$interval/g" < run_metacells_filter.sh | bsub
+else
+    sed -e "s/input/$input/g" \
+        -e "s/min_size/$min_size/g" \
+        -e "s/max_size/$max_size/g" \
+        -e "s/interval/$interval/g" < run_metacells.sh | bsub

931adbf4db012bb9423e5dca7eaa63e887812d3f
margaretAlexander <98061354+margaretAlexander@users.noreply.github.com>
2026-08-24 11:21:40 -0500

    Refactor metacell script to include lateral filter

--- sender_metacell.sh
+++ sender_metacell.sh
@@ -3,15 +3,10 @@ input=$(echo $1) # dataset to run metacell on
 min_size=$(echo $2) # min metacell size
 max_size=$(echo $3) # max metacell size
 interval=$(echo $4) # interval between metacell sizes
-filter_lateral=$(echo $5) # true/false
+filter_lateral=$(echo $5) # whether to filter lateral genes
 
-if [[filter_lateral]]; then
-    sed -e "s/input/$input/g" \
-        -e "s/min_size/$min_size/g" \
-        -e "s/max_size/$max_size/g" \
-        -e "s/interval/$interval/g" < run_metacells_filter.sh | bsub
-else
-    sed -e "s/input/$input/g" \
-        -e "s/min_size/$min_size/g" \
-        -e "s/max_size/$max_size/g" \
-        -e "s/interval/$interval/g" < run_metacells.sh | bsub
+sed -e "s/input/$input/g" \
+    -e "s/min_size/$min_size/g" \
+    -e "s/max_size/$max_size/g" \
+    -e "s/interval/$interval/g" \
+    -e "s/filter_lateral/$filter_lateral/g" < run_metacells_test.sh | bsub
